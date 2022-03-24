a0c4f820e0b51c17729c8dbfccba4795e0e0b08a
Melanie <phe.melaniee@gmail.com>
2022-03-24 09:56:46 +0100

    Added direction images for player

--- src/Player.java
+++ src/Player.java
@@ -10,15 +10,23 @@ import static java.awt.event.KeyEvent.*;
 
 public class Player {
     private Vector2D pos;
-    private final BufferedImage image;
-    
-    
+    private BufferedImage image;
+    private final BufferedImage[] directionImage = new BufferedImage[8];
     
     public Player(double x, double y) {
         pos = new Vector2D(x,y);
         image = Image.getFrom("resources/images/character.png");
+        //directionImage counterclockwise
+        directionImage[0] = Image.getFrom("resources/images/playerImages/top.jpg");
+        directionImage[1] = Image.getFrom("resources/images/playerImages/left.jpg");
+        directionImage[2] = Image.getFrom("resources/images/playerImages/bottom.png");
+        directionImage[3] = Image.getFrom("resources/images/playerImages/right.png");
+        directionImage[4] = Image.getFrom("resources/images/playerImages/top_left.png");
+        directionImage[5] = Image.getFrom("resources/images/playerImages/bottom_left.png");
+        directionImage[6] = Image.getFrom("resources/images/playerImages/bottom_right.png");
+        directionImage[7] = Image.getFrom("resources/images/playerImages/top_right.jpg");
     }
-    
+
     public void draw(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
         AffineTransform old = g2d.getTransform();
@@ -34,15 +42,19 @@ public class Player {
     public void movement() {
         if(Keyboard.isPressed(VK_UP) || Keyboard.isPressed(VK_W)) {
             pos.add(0,-10);
+            image = directionImage[0];
         }
         if(Keyboard.isPressed(VK_DOWN) || Keyboard.isPressed(VK_S)) {
             pos.add(0,10);
+            image = directionImage[2];
         }
         if(Keyboard.isPressed(VK_RIGHT) || Keyboard.isPressed(VK_D)) {
             pos.add(10,0);
+            image = directionImage[3];
         }
         if(Keyboard.isPressed(VK_LEFT) || Keyboard.isPressed(VK_A)){
             pos.add(-10,0);
+            image = directionImage[1];
         }
     }
 }

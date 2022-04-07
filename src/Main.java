8ded0c835d23c75166389ff5868fef2845dbbce5
Louis Lafontaine <louishenri.lafontaine@gmail.com>
2022-04-07 10:12:02 +0200

    Revert "fixing"
    This reverts commit 6e87b4d46fc98805870926711b85a884784051e1.

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,8 @@ import util.Sound;
 
 public class Main {
     public static void main(String[] args) {
-//        Sound sound = new Sound("sounds/musics/xDeviruchi/Title Theme.mp3");
-//        sound.play();
+        Sound sound = new Sound("sounds/musics/xDeviruchi/Title Theme.mp3");
+        sound.play();
         MainWindow.get().init();
     }
 }
